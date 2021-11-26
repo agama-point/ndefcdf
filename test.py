ef8314bbb9cb0c6db90e1b042a166e340aed970d
Petr Kracík <petrkr@petrkr.net>
2021-11-26 18:26:47 +0100

    Updated lib according to CDF-003

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import ndefcdf
 
 pay = ndefcdf.lnpay.CDFLNPay("https://example.org/payurl")
-pay_label = ndefcdf.lnpay.CDFLNPay("Label 1:https://example.org/payurl")
+pay_label = ndefcdf.lnpay.CDFLNPay("Label 1;https://example.org/payurl")
 print(pay)
 print(pay_label)
 
-wit = ndefcdf.lnwithdrawal.CDFLNWithdrawal("https://example.org/withdrawal")
-wit_label = ndefcdf.lnwithdrawal.CDFLNWithdrawal("Label 1:https://example.org/withdrawal")
+wit = ndefcdf.lnwithdrawal.CDFLNWithdrawal("https;//example.org/withdrawal")
+wit_label = ndefcdf.lnwithdrawal.CDFLNWithdrawal("Label 1;https://example.org/withdrawal")
 print(wit)
 print(wit_label)
 
